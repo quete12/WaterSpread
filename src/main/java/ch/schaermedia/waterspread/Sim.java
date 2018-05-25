8760c456dac0d266166aae83ecb14546b024e3da
Quentin <Quentin@PC-5>
2018-05-25 13:41:49 +0200

    some comments

--- src/main/java/ch/schaermedia/waterspread/Sim.java
+++ src/main/java/ch/schaermedia/waterspread/Sim.java
@@ -102,16 +102,22 @@ public class Sim extends PApplet {
         float value = grid[VALUE_LAYER_IDX][x][y];
         int idx = x + y * cols;
         if (value < 0) {
+            // Render pixels with a negative (invalid) value in blue
             g.pixels[idx] = color(0, 0, 255);
-//        } else if (value - grid[AVERAGE_LAYER_IDX][x][y] < 0) {
+        } else if (value - grid[AVERAGE_LAYER_IDX][x][y] < 0) {
+            // For debugging render pixels with a lower than average value in red
+
 //            g.pixels[idx] = color(255, 0, 0);
         } else {
+            // Render the pixel in a rage from black to grey
             g.pixels[idx] = color(value);
         }
     }
 
     private void update(int x, int y) {
+        // Adds the value flowing in from other cells and subtracts outwards flow
         grid[VALUE_LAYER_IDX][x][y] = grid[VALUE_LAYER_IDX][x][y] + grid[INBOUND_LAYER_IDX][x][y] - grid[OUTBOUND_LAYER_IDX][x][y];
+        // Reset in and out flow
         grid[INBOUND_LAYER_IDX][x][y] = 0;
         grid[OUTBOUND_LAYER_IDX][x][y] = 0;
     }
@@ -120,14 +126,11 @@ public class Sim extends PApplet {
 
         float value = grid[VALUE_LAYER_IDX][x][y];
 
-        float average;
-        float tospread = 0;
-        int range = 2;
-        average = calcAverage(x, y, range);
+        int range = 3;
+
+        float average = calcAverage(x, y, range);
+        float tospread = value - average;
         grid[AVERAGE_LAYER_IDX][x][y] = average;
-        tospread = value - average;
-        range++;
-        //tospread = (float) (tospread > 0 ? tospread : value > diffTotal ? diffTotal * 0.1 : 0);
         if (tospread <= 0) {
             return;
         }
